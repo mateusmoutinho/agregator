d5ec3420c6be266950bc1172f09946c2e014d94e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-01 02:58:06 -0300

    att

---
+++ src/flags.lua
@@ -0,0 +1,3 @@
+PATHS = {"path","p"}
+ENTRIES = {"entrie", "e"}
+OUTPUT = {"output", "o"}
--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,19 @@
 relative_load("agregator.lua")
+relative_load("flags.lua")
 
 function main()
-    local paths_size = argv.get_flag_size({ "path", "p" })
+    local paths_size = argv.get_flag_size(PATHS)
     local paths = {}
     for i=1,paths_size do 
-        paths[i] = argv.get_flag_arg_by_index({ "path", "p" }, i)
+        paths[i] = argv.get_flag_arg_by_index(PATHS, i)
     end 
 
     local entrie_content = ""
-    local eentrie_file = argv.get_flag_arg_by_index({ "entrie", "e" }, 1)
+    local eentrie_file = argv.get_flag_arg_by_index(ENTRIES, 1)
     if entrie_file then 
         entrie_content = dtw.load_file(eentrie_file)
     end
-    local output = argv.get_flag_arg_by_index({ "output", "o" }, 1)
+    local output = argv.get_flag_arg_by_index(OUTPUT, 1)
     if not output then 
         error("Output file is required")
     end
