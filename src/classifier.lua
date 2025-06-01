6a9bf3e2ecc8aa7a532985f9339a54a1a901066f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-01 08:55:59 -0300

    att

--- src/classifier.lua
+++ src/classifier.lua
@@ -1,44 +1,41 @@
 -- return true if good and false if bad or nil if not sure
-function clasify_modification_once(old,new,goal_instructions,classifier_model)
-
+function clasify_modification_once(props)
     local MAX_TRIES = 5
-    for i =1,MAX_TRIES do 
-        local llm = newLLM({model=classifier_model})
+    for i = 1, MAX_TRIES do
+        local llm = newLLM({model = props.classifier_model})
 
         llm.add_system_prompt([[
             you are a classifier that determines if a modification is good or bad based on the goal instructions provided.
             use the function set_as_good() to set the modification as good and set_as_bad() to set it as bad.
             you must use the function set_as_good() or set_as_bad() to classify the modification.
-            set as bad if usefull contennt wore removed,
+            set as bad if useful content was removed,
             set as bad if redundant information was added 
-            set as bad if non related information to the goals were setted
+            set as bad if non-related information to the goals was set
         ]])
-        llm.add_user_prompt("old: " .. old.."\n")
-        llm.add_user_prompt("new: " .. new .. "\n")
-        llm.add_user_prompt("goal instructions: " .. goal_instructions .. "\n")
+        llm.add_user_prompt("old: " .. props.old .. "\n")
+        llm.add_user_prompt("new: " .. props.new .. "\n")
+        llm.add_user_prompt("goal instructions: " .. props.goal_instructions .. "\n")
         local is_good = nil
 
-        llm.add_function("set_as_good","set as good",{}, function()
+        llm.add_function("set_as_good", "set as good", {}, function()
             is_good = true
         end)
-        llm.add_function("set_as_bad","set as bad",{}, function()
+        llm.add_function("set_as_bad", "set as bad", {}, function()
             is_good = false
         end)
 
         llm.generate()
-        if is_good ~= nil then 
+        if is_good ~= nil then
             return is_good
         end
-    end 
+    end
     error("Classifier failed to classify the modification after " .. MAX_TRIES .. " tries.")
 end
-
-function clasify_modification(old, new, goal_instructions, total_classifications, min_positive_percent, classifier_model)
+function clasify_modification(props)
     local positive_count = 0
 
-    for i = 1, total_classifications do
-        
-        local result = clasify_modification_once(old, new, goal_instructions, classifier_model)
+    for i = 1, props.total_classifications do
+        local result = clasify_modification_once(props.old, props.new, props.goal_instructions, props.classifier_model)
         if result == true then
             print(string.format("Classification %d: Positive", i))
             positive_count = positive_count + 1
@@ -48,8 +45,7 @@ function clasify_modification(old, new, goal_instructions, total_classifications
         end
     end
 
-    local positive_percent = (positive_count / total_classifications) * 100
-    print(string.format("Positive classifications: %d/%d (%.2f%%)", positive_count, total_classifications, positive_percent))
-    return positive_percent >= min_positive_percent
+    local positive_percent = (positive_count / props.total_classifications) * 100
+    print(string.format("Positive classifications: %d/%d (%.2f%%)", positive_count, props.total_classifications, positive_percent))
+    return positive_percent >= props.min_positive_percent
 end
-
